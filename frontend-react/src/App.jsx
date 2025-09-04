6e9c859374f6957eab4b8f78c933030c0d4aa7c2
kushbharti <try.kushbharti@gmail.com>
2025-09-05 01:54:06 +0530

    registration

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,9 +1,11 @@
 // import { useState } from 'react'
 import './assets/css/style.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
 import Main from './components/Main'
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Register from './components/Register'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Login from './components/Login'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -11,9 +13,15 @@ function App() {
   return (
     <>
 
+      <BrowserRouter>
       <Header />
-      <Main />
-      <Footer />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path ='/register' element={<Register />} />
+          <Route path='/login' element={<Login/>}  />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
 
     </>
   )
--- frontend-react/src/components/Button.jsx
+++ frontend-react/src/components/Button.jsx
@@ -1,37 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 
 const Button = (probs) => {
   return (
     <>
-        <a className={`btn ${probs.class}`} href="">{probs.text}</a>
+        <Link className={`btn ${probs.class}`} to={probs.url}>{probs.text}</Link>
     </>
   )
 }
 
 export default Button
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
