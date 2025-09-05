28a0589b772c23afb79da78668c0b136579c1398
kushbharti <try.kushbharti@gmail.com>
2025-09-05 20:02:32 +0530

    Commit

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'spp',                
         'USER': 'root',               
-        'PASSWORD': '#513523224',            
+        'PASSWORD': '#',            
         'HOST': '127.0.0.1',                 
         'PORT': '3306', 
     } 
